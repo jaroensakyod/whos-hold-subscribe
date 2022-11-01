ccd8d96830f62bbf05834cacf4131fd628f01c45
jaroensakyod <jaroensak.yod@gmail.com>
2022-11-01 22:14:31 +0700

    link2

--- pages/checkdata.js
+++ pages/checkdata.js
@@ -13,7 +13,7 @@ export default function Succ() {
           const verifyHolder = async (nftAddress, ownerDiscordId, discordGuildId, ownerWalletAddress, projectName, totalSupply, symbol, website, facebook ,twitter, discordInviteLink, etherscan, planId, roleName) => {
                     try {
                               const response = await axios.post(
-                                        `https://alert-puce-rabbit.cyclic.app/v1/subscription/subscribe/`,
+                                        `https://charming-puce-tutu.cyclic.app/v1/subscription/subscribe/`,
                                         {
                                                 nftAddress: nftAddress,
                                                 ownerDiscordId: ownerDiscordId,
